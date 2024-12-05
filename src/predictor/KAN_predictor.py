8dd6de7b336117f08692f52eb3ff1cc0bf38528f
Lzh-Function <takuho2002@outlook.jp>
2024-12-05 19:27:41 +0900

    update

--- src/predictor/KAN_predictor.py
+++ src/predictor/KAN_predictor.py
@@ -23,6 +23,7 @@ class KAN_predictor(nn.Module):
         self.device = device
         self.dir = working_dir
         self.results_dir_name = results_dir_name
+        os.makedirs(self.dir / self.results_dir_name, exist_ok=True)
         self.LOGGER = LOGGER
         self.dtype = torch.get_default_dtype()
 
@@ -75,7 +76,7 @@ class KAN_predictor(nn.Module):
                 
 
     def train(self, tr_loader, val_loader, lr, lamb,
-              reg_metric="edge_forward_spline_n", lamb_l1=1., lamb_entropy=1., lamb_coefdiff=0., lamb_coef=0., test=False, fold_idx=None):
+              reg_metric="edge_forward_spline_n", lamb_l1=1., lamb_entropy=1., lamb_coefdiff=0., lamb_coef=0., test=False, test_with_grids_refine=False, fold_idx=None):
         
         # init model
         self.model = KAN(width=self.width, grid=5, k=3, device=self.device, seed=42, auto_save=False)
@@ -98,7 +99,8 @@ class KAN_predictor(nn.Module):
 
         pbar = tqdm(range(self.epochs), desc="train_start", dynamic_ncols=True)
         est_cnt = 0
-        best_val_loss = float("inf")
+        if not (test_with_grids_refine and "best_val_loss" in self.__dict__.keys()):
+            self.best_val_loss = float("inf")
 
         # training & validation
         # torch.manual_seed(42) # fix shuffle for tr_loader
@@ -171,24 +173,29 @@ class KAN_predictor(nn.Module):
                 self.LOGGER.debug(f"epoch {_} | tr loss: {results['tr_loss'][-1]:.2e} | tr r2: {results['tr_metrics']["r2_score"][-1]:.2e} | val loss: {results["val_loss"][-1]:.2e} | val r2: {results['val_metrics']["r2_score"][-1]:.2e}")
                 
             # EarlyStopping
-            if best_val_loss == float("inf"):
-                best_val_loss = results["val_loss"][-1]
+            if self.best_val_loss == float("inf"):
+                self.best_val_loss = results["val_loss"][-1]
                 results["best_epoch"] = _
-                if test and fold_idx is not None:
+                if (test or test_with_grids_refine) and fold_idx is not None:
                     torch.save(self.model.state_dict(), self.dir / self.results_dir_name / "best_models" / f"fold_{fold_idx}.pth")
-            elif best_val_loss - results["val_loss"][-1] > best_val_loss * 0.001:
-                best_val_loss = results["val_loss"][-1]
+            elif self.best_val_loss - results["val_loss"][-1] > self.best_val_loss * 0.001:
+                self.best_val_loss = results["val_loss"][-1]
                 results["best_epoch"] = _
                 est_cnt = 0
-                if test and fold_idx is not None:
+                if (test or test_with_grids_refine) and fold_idx is not None:
                     torch.save(self.model.state_dict(), self.dir / self.results_dir_name / "best_models" / f"fold_{fold_idx}.pth")
             else:
                 est_cnt += 1
             if est_cnt == self.patience:
                 self.LOGGER.info(f"EarlyStopping at epoch {_}")
                 pbar.close()
+                if test:
+                    del self.best_val_loss
                 break
-        
+
+            if _ == self.epochs-1 and test:
+                del self.best_val_loss
+
         return results
 
 
@@ -245,6 +252,7 @@ class KAN_predictor(nn.Module):
                 results = self.train(tr_loader, val_loader, lr=lr, lamb=lamb, test=True, fold_idx=i)
 
             # test
+            self.model.load_state_dict(torch.load(self.dir / self.results_dir_name / "best_models" / f"fold_{i}.pth"))
             with torch.no_grad():
                 pred_list = []
                 for x, y in test_loader:
@@ -303,6 +311,119 @@ class KAN_predictor(nn.Module):
         df.to_csv(self.dir / self.results_dir_name / "test_scores.csv")
         
         self.LOGGER.info("|-----TEST FINISHED-----|")
+    
+    def test_with_grids_refine(self, x_test, y_test, lr=None, lamb=None):
+        self.x_test = x_test
+        self.y_test = y_test
+
+        self.LOGGER.info("|-----TEST(emsemble) with grids refine-----|")
+
+        # make results dirs
+        os.makedirs(self.dir / self.results_dir_name / "best_models", exist_ok=True)
+        os.makedirs(self.dir / self.results_dir_name / "train_history", exist_ok=True)
+        
+        # get best params
+        if lr is None and lamb is None:
+            best_params = self.study.best_params
+
+        # preparing test data
+        test = TensorDataset(torch.from_numpy(self.x_test).type(self.dtype), torch.from_numpy(self.y_test).type(self.dtype))
+        test_loader = DataLoader(dataset=test, batch_size=self.batch_size, shuffle=False, num_workers=8, pin_memory=True)
+
+        # CrossValidation training
+        for i, (tr_idx, val_idx) in enumerate(self.kf.split(self.x_tr_val, self.y_tr_val)):
+            self.LOGGER.info(f"-----Fold-{i}-----")
+
+            # preparing train data
+            x_tr, x_val, y_tr, y_val = self.x_tr_val[tr_idx], self.x_tr_val[val_idx], self.y_tr_val[tr_idx], self.y_tr_val[val_idx]
+            tr = TensorDataset(torch.from_numpy(x_tr).type(self.dtype), torch.from_numpy(y_tr).type(self.dtype))
+            val = TensorDataset(torch.from_numpy(x_val).type(self.dtype), torch.from_numpy(y_val).type(self.dtype))
+            tr_loader = DataLoader(dataset=tr, batch_size=self.batch_size, shuffle=True, num_workers=8, pin_memory=True)
+            val_loader = DataLoader(dataset=val, batch_size=self.batch_size, shuffle=False, num_workers=8, pin_memory=True)
+
+            # train
+            grids = [10, 20]
+            if lr is None and lamb is None:
+                results = self.train(tr_loader, val_loader, **best_params, test_with_grids_refine=True, fold_idx=i)
+                self.LOGGER.info(f"---grids refine 5 -> 10")
+                self.model.refine(new_grid=grids[0])
+                results2 = self.train(tr_loader, test_loader, **best_params, test_with_grids_refine=True, fold_idx=i)
+                self.LOGGER.info(f"---grids refine 10 -> 20")
+                self.model.refine(new_grid=grids[1])
+                results3 = self.train(tr_loader, test_loader, **best_params, test_with_grids_refine=True, fold_idx=i)
+            else:
+                results = self.train(tr_loader, val_loader, lr=lr, lamb=lamb, test_with_grids_refine=True, fold_idx=i)
+                self.LOGGER.info(f"---grids refine 5 -> 10")
+                self.model.refine(new_grid=grids[0])
+                results2 = self.train(tr_loader, test_loader, lr=lr, lamb=lamb, test_with_grids_refine=True, fold_idx=i)
+                self.LOGGER.info(f"---grids refine 10 -> 20")
+                self.model.refine(new_grid=grids[1])
+                results3 = self.train(tr_loader, test_loader, lr=lr, lamb=lamb, test_with_grids_refine=True, fold_idx=i)
+
+            # test
+            self.model.load_state_dict(torch.load(self.dir / self.results_dir_name / "best_models" / f"fold_{i}.pth"))
+            with torch.no_grad():
+                pred_list = []
+                for x, y in test_loader:
+                    x = x.to(self.device)
+                    pred = self.forward(x)
+                    pred_list.append(pred)
+                pred = torch.cat(pred_list)
+                if i == 0:
+                    test_pred = pred
+                else:
+                    test_pred = test_pred + pred
+            
+            # saving train history graph
+            fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+            ax1 = axes[0]
+            ax2 = axes[1]
+            ax1.plot(results["tr_loss"]+results2["tr_loss"]+results3["tr_loss"], label="train")
+            ax1.plot(results["val_loss"]+results2["val_loss"]+results3["val_loss"], label="valid")
+            ax1.set_xlabel("epoch")
+            ax1.set_ylim([0, 1])
+            ax1.legend()
+            if self.mode == "classification":
+                ax1.set_title(f"Loss (logloss)")
+                ax2.plot(results["tr_metrics"]["roc_auc_score"]+results2["tr_metrics"]["roc_auc_score"]+results3["tr_metrics"]["roc_auc_score"], label="train")
+                ax2.plot(results["val_metrics"]["roc_auc_score"]+results2["val_metrics"]["roc_auc_score"]+results3["val_metrics"]["roc_auc_score"], label="valid")
+                ax2.set_xlabel("epoch")
+                ax2.set_ylim([0, 1])
+                ax2.legend()
+                ax2.set_title("AUROC")
+            else:
+                ax1.set_title(f"Loss (MSE)")
+                ax2.plot(results["tr_metrics"]["r2_score"]+results2["tr_metrics"]["r2_score"]+results3["tr_metrics"]["r2_score"], label="train")
+                ax2.plot(results["val_metrics"]["r2_score"]+results2["val_metrics"]["r2_score"]+results3["val_metrics"]["r2_score"], label="valid")
+                ax2.set_xlabel("epoch")
+                ax2.set_ylim([0, 1])
+                ax2.legend()
+                ax2.set_title("R2")
+            plt.tight_layout()
+            plt.savefig(self.dir / self.results_dir_name / "train_history" / f"fold_{i}_grids_refine.png")
+            plt.close()
+
+            # delete best_val_loss
+            del self.best_val_loss
+        
+        self.LOGGER.info(f"-----{self.fold}-Fold finished-----")
+
+        # emsemble, calculating test scores
+        self.LOGGER.info("-----scores-----")
+        df = pd.DataFrame(columns=[f.__name__ for f in self.metrics])
+        test_pred = (test_pred / self.fold).cpu().detach().numpy()
+        test_results = {}
+        for f in self.metrics:
+            if f.__name__ == "roc_auc_score" or f.__name__ == "r2_score" or f.__name__ == "root_mean_squared_error":
+                test_results[f.__name__] = f(self.y_test, test_pred)
+            else:
+                test_results[f.__name__] = f(self.y_test, np.round(np.clip(test_pred, 0, 1)))
+            self.LOGGER.info(f"{f.__name__}: {test_results[f.__name__]:.2e}")
+            df.loc[0, f.__name__] = f"{test_results[f.__name__]:.2e}"
+        df.to_csv(self.dir / self.results_dir_name / "test_scores_grids_refine.csv")
+        
+        self.LOGGER.info("|-----TEST FINISHED-----|")
+
 
 
 if __name__ == "__main__":
