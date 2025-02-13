08ddf56b30c41b2f0ddafe2f515f992cbeeb739b
Lzh-Function <takuho2002@outlook.jp>
2025-02-14 00:02:36 +0900

    fix & improve

--- molkan/experiments/Attentive_FP/250213_BACE_grid_search.py
+++ molkan/experiments/Attentive_FP/250213_BACE_grid_search.py
@@ -18,7 +18,7 @@ import sys
 project_path = os.path.abspath(__file__).split("/experiments")[0]
 sys.path.append(project_path)
 
-from src.utils import init_logger
+from src.utils import init_logger, fix_seed
 from src.AttentiveFP.data_handler import AttentiveFPDatasets, split_dataset, prep_threeAttentiveFPDataLoader
 from src.AttentiveFP.trainer import AttentiveFP_Trainer
 
@@ -43,6 +43,7 @@ if __name__ == "__main__":
 
     logger = init_logger(outdir)
     
+    fix_seed(config.seed, fix_gpu=True)
     # prepare data
     logger.info("Preparing data...")
     df = pd.read_csv(os.path.join(project_path, "data", "tox_csv", "bace.csv"), index_col=0)
@@ -50,10 +51,13 @@ if __name__ == "__main__":
     y = np.array(df["Class"])
     datasets = AttentiveFPDatasets(smiles, y, use_chirality=True)
     trainset, validset, testset = split_dataset(datasets, stratify=True, seed=config.seed)
+    logger.info(f"train: {len(trainset)}, valid: {len(validset)}, test: {len(testset)}")
     trainloader, validloader, testloader = prep_threeAttentiveFPDataLoader(trainset, validset, testset, batch_size=32, shuffle=True, num_workers=8, pin_memory=True)
 
+    # Grid Search
     trainloss_dict = {}
     validloss_dict = {}
+    testscores_dict = {}
 
     lrs = [1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5]
     grids = [1, 2, 4, 8]
@@ -78,10 +82,12 @@ if __name__ == "__main__":
 
                         trainloss_dict[config.note] = train_losses
                         validloss_dict[config.note] = valid_losses
+                        testscores_dict[config.note] = test_scores
                 else:
                     config.use_KAN_predictor = use_KAN_predictor
                     config.use_KAN_embed = use_KAN_embed
                     config.lr = lr
+                    config.num_grids = None
                     config.note = f"use_KAN_embed_{use_KAN_embed}_use_KAN_predictor_{use_KAN_predictor}_lr_{lr}"
 
                     logger.info(f"----- {config.note} -----")
@@ -92,38 +98,39 @@ if __name__ == "__main__":
 
                     trainloss_dict[config.note] = train_losses
                     validloss_dict[config.note] = valid_losses
+                    testscores_dict[config.note] = test_scores
         
-        # save the progress
-        logger.info("Saving the progress...")
-        progress_dir = os.path.join(outdir, "progress")
-        os.makedirs(progress_dir, exist_ok=True)
-        if use_KAN_predictor or use_KAN_embed:
-            fig, ax = plt.subplots(4, 4, figsize=(32, 24))
-            fig.suptitle(f"Grid Search for use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}", fontsize=24)
-            epoch = list(range(1, config.epoch+1))
-            for i, (key, value) in enumerate(trainloss_dict.items()):
-                ax[i//4, i%4].plot(epoch, value, label="train loss")
-                ax[i//4, i%4].plot(epoch, validloss_dict[key], label="valid loss")
-                ax[i//4, i%4].set_title(key)
-                ax[i//4, i%4].set_xlabel("epoch")
-                ax[i//4, i%4].set_ylabel("loss")
-                ax[i//4, i%4].legend()
-            plt.tight_layout()
-            fig.savefig(os.path.join(progress_dir, f"Grid_Search_use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}.png"))
-            plt.close()
-        else:
-            fig, ax = plt.subplots(1, 4, figsize=(32, 6))
-            fig.suptitle(f"Grid Search for use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}", fontsize=24)
-            epoch = list(range(1, config.epoch+1))
-            for i, (key, value) in enumerate(trainloss_dict.items()):
-                ax[i].plot(epoch, value, label="train loss")
-                ax[i].plot(epoch, validloss_dict[key], label="valid loss")
-                ax[i].set_title(key)
-                ax[i].set_xlabel("epoch")
-                ax[i].set_ylabel("loss")
-                ax[i].legend()
-            plt.tight_layout()
-            plt.savefig(os.path.join(progress_dir, f"Grid_Search_use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}.png"))
+            # save the progress
+            logger.info("Saving the progress...")
+            progress_dir = os.path.join(outdir, "progress")
+            os.makedirs(progress_dir, exist_ok=True)
+            if use_KAN_predictor or use_KAN_embed:
+                fig, ax = plt.subplots(4, 4, figsize=(32, 24))
+                fig.suptitle(f"Grid Search for use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}", fontsize=24)
+                epoch = list(range(1, config.epoch+1))
+                for i, (key, value) in enumerate(trainloss_dict.items()):
+                    ax[i//4, i%4].plot(epoch, value, label="train loss")
+                    ax[i//4, i%4].plot(epoch, validloss_dict[key], label="valid loss")
+                    ax[i//4, i%4].set_title(f"{key} - testAUROC:{testscores_dict[key]["AUROC"]:.3f}")
+                    ax[i//4, i%4].set_xlabel("epoch")
+                    ax[i//4, i%4].set_ylabel("loss")
+                    ax[i//4, i%4].legend()
+                plt.tight_layout()
+                fig.savefig(os.path.join(progress_dir, f"Grid_Search_use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}.png"))
+                plt.close()
+            else:
+                fig, ax = plt.subplots(1, 4, figsize=(32, 6))
+                fig.suptitle(f"Grid Search for use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}", fontsize=24)
+                epoch = list(range(1, config.epoch+1))
+                for i, (key, value) in enumerate(trainloss_dict.items()):
+                    ax[i].plot(epoch, value, label="train loss")
+                    ax[i].plot(epoch, validloss_dict[key], label="valid loss")
+                    ax[i].set_title(f"{key} - testAUROC:{testscores_dict[key]["AUROC"]:.3f}")
+                    ax[i].set_xlabel("epoch")
+                    ax[i].set_ylabel("loss")
+                    ax[i].legend()
+                plt.tight_layout()
+                plt.savefig(os.path.join(progress_dir, f"Grid_Search_use_KAN_predictor_{use_KAN_predictor}_use_KAN_embed_{use_KAN_embed}.png"))
 
     logger.info("All experiments finished.")
 
